6957ad58885d5df1941b989540b0bf2a7692ae4a
Muhammed Zeba <parice02@hotmail.com>
2023-12-20 21:36:09 +0000

    add test

---
+++ test/test.py
@@ -0,0 +1,21 @@
+from sms import get_credit, get_sms_status, send_sms
+
+base_url = "https://www.aqilas.com/api/v1/"
+token = "XXXXX"
+
+
+# To find out how much credit you have left.
+print(get_credit(base_url, token))
+
+# To send a message
+print(
+    send_sms(
+        base_url,
+        token,
+        receivers=["+226XXX", "+226XXX", "+226XXX", "+226XXX"],
+        content="send test sms for ETIMBRE",
+    )
+)
+
+# To check the status of a message
+print(get_sms_status(base_url, token, "SMS BULK ID"))
