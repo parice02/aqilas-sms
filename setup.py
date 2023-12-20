e5e4f7a51a699066f579df56734d9e6dbf393b57
Muhammed Zeba <parice02@hotmail.com>
2023-12-20 21:07:50 +0000

    initialize git repo

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aqilas-sms",
+    version="0.0.1",
+    description="",
+    packages=find_packages(),
+    author="Mohamed Zeba, Abdoul Kader Kaboré",
+    author_email="<parice02@hotmail.com>, <derokabore@gmail.com>",
+    zip_safe=False,
+)
---
+++ sms/__init__.py
@@ -0,0 +1,41 @@
+import requests
+
+
+
+credits_url = "credit"
+sms = "sms"
+
+
+def get_credit(url: str, token: str):
+    v = url + credits_url if url[-1] == "/" else url + "/" + credits_url
+    headers = {
+        "content-type": "application/json",
+        "X-AUTH-TOKEN": token,
+    }
+    return requests.get(v, headers=headers).json()
+
+
+def send_sms(
+    url: str,
+    token: str,
+    sender: str = "ETIMBRE",
+    receivers: list = [],
+    content: str = "",
+):
+    v = url + sms if url[-1] == "/" else url + "/" + sms
+    headers = {
+        "content-type": "application/json",
+        "X-AUTH-TOKEN": token,
+    }
+    data = {"from": sender, "to": receivers, "text": content}
+    return requests.post(v, headers=headers, json=data).json()
+
+
+def get_sms_status(url: str, token: str, bulkid: str):
+    v = url + sms if url[-1] == "/" else url + "/" + sms
+    v = v + "/" + bulkid
+    headers = {
+        "content-type": "application/json",
+        "X-AUTH-TOKEN": token,
+    }
+    return requests.get(v, headers=headers).json()
