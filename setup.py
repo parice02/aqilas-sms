12525d53d87bda1a01fbc0967ab7076d057649c7
Muhammed Zeba <parice02@hotmail.com>
2024-06-01 09:13:51 +0000

    update

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
 from pathlib import Path
 from setuptools import setup, find_packages
 
+BASE_DIR = Path(__file__).resolve().parent
+with open(BASE_DIR / "LICENSE") as file:
+    _license = file.read()
+
+with open(BASE_DIR / "README.md") as file:
+    description = file.read()
+
 setup(
     name="aqilas-sms",
     version="0.0.1",
-    description="",
-    packages=find_packages(),
-    author="Mohamed Zeba, Abdoul Kader Kaboré",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/parice02/aqilas-sms",
+    description="Library for sending sms",
+    packages=find_packages(include=["sms"]),
+    author="Mohamed Zeba",
     author_email="<parice02@hotmail.com>",
-    zip_safe=False,
+    license=_license,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    requires=["request"],
 )
